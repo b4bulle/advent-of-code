8543cdf8b884f27e35e4b7121be5fe265ad3580e
b4bulle <37732841+b4bulle@users.noreply.github.com>
2025-12-04 19:47:13 +0100

    visualization

--- 2025/Babulle.AdventOfCode2025.Day4/Babulle.AdventOfCode2025.Day4/Program.cs
+++ 2025/Babulle.AdventOfCode2025.Day4/Babulle.AdventOfCode2025.Day4/Program.cs
@@ -7,82 +7,96 @@ var lineSize = lines[0].Length;
 
 var result = 0;
 var iterationResult = 1;
+var iterations = 0;
 
-while (iterationResult != 0)
+using (var fileStream = File.Open("output.txt", FileMode.Create))
+using (var writer = new StreamWriter(fileStream))
 {
-    iterationResult = 0;
-    var table = new int[lineCount, lineSize];
-
-    for (var i = 0; i < lineCount; ++i)
+    while (iterationResult != 0)
     {
-        for (var j = 0; j < lineSize; ++j)
+        ++iterations;
+        writer.WriteLine(iterations);
+        iterationResult = 0;
+        var table = new int[lineCount, lineSize];
+
+        for (var i = 0; i < lineCount; ++i)
         {
-            if (lines[i][j] == '@')
+            for (var j = 0; j < lineSize; ++j)
             {
-                if (i > 0 && j > 0)
+                if (lines[i][j] == '@')
                 {
-                    ++table[i - 1, j - 1];
-                }
-
-                if (i > 0)
-                {
-                    ++table[i - 1, j];
-                }
-
-                if (i > 0 && j < lineSize - 1)
-                {
-                    ++table[i - 1, j + 1];
-                }
-
-                if (j > 0)
-                {
-                    ++table[i, j - 1];
-                }
-
-                if (j < lineSize - 1)
-                {
-                    ++table[i, j + 1];
-                }
-
-                if (i < lineCount - 1 && j > 0)
-                {
-                    ++table[i + 1, j - 1];
-                }
-
-                if (i < lineCount - 1)
-                {
-                    ++table[i + 1, j];
-                }
-
-                if (i < lineCount - 1 && j < lineSize - 1)
-                {
-                    ++table[i + 1, j + 1];
+                    IncrementSurroundings(i, j, table, lineSize, lineCount);
                 }
             }
         }
-    }
 
-    for (var i = 0; i < lineCount; ++i)
-    {
-        var stringBuilder = new StringBuilder(lines[i]);
+        for (var i = 0; i < lineCount; ++i)
+        {
+            var stringBuilder = new StringBuilder(lines[i]);
+            
+            for (var j = 0; j < lineSize; ++j)
+            {
+                //Console.Write(table[i, j] + "");
+                if (lines[i][j] == '@')
+                {
+                    if (table[i, j] < 4)
+                    {
+                        ++iterationResult;
+                        stringBuilder[j] = '.';
+                    }
+                }
+            }
+            writer.WriteLine(lines[i]);
+            lines[i] = stringBuilder.ToString();
+        }
         
-        for (var j = 0; j < lineSize; ++j)
-        {
-            //Console.Write(table[i, j] + "");
-            if (lines[i][j] == '@')
-            {
-                if (table[i, j] < 4)
-                {
-                    ++iterationResult;
-                    stringBuilder[j] = '.';
-                }
-            }
-        }
-        //Console.WriteLine();
-        lines[i] = stringBuilder.ToString();
+        result += iterationResult;
+        writer.WriteLine();
     }
-    
-    result += iterationResult;
 }
 
 Console.WriteLine(result);
+return;
+
+void IncrementSurroundings(int i1, int i2, int[,] ints, int lineSize1, int lineCount1)
+{
+    if (i1 > 0 && i2 > 0)
+    {
+        ++ints[i1 - 1, i2 - 1];
+    }
+
+    if (i1 > 0)
+    {
+        ++ints[i1 - 1, i2];
+    }
+
+    if (i1 > 0 && i2 < lineSize1 - 1)
+    {
+        ++ints[i1 - 1, i2 + 1];
+    }
+
+    if (i2 > 0)
+    {
+        ++ints[i1, i2 - 1];
+    }
+
+    if (i2 < lineSize1 - 1)
+    {
+        ++ints[i1, i2 + 1];
+    }
+
+    if (i1 < lineCount1 - 1 && i2 > 0)
+    {
+        ++ints[i1 + 1, i2 - 1];
+    }
+
+    if (i1 < lineCount1 - 1)
+    {
+        ++ints[i1 + 1, i2];
+    }
+
+    if (i1 < lineCount1 - 1 && i2 < lineSize1 - 1)
+    {
+        ++ints[i1 + 1, i2 + 1];
+    }
+}
