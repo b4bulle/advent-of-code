2f32e59c6913a8d50ee75c0f645bb2b7c59bbe13
b4bulle <37732841+b4bulle@users.noreply.github.com>
2025-12-10 16:20:52 +0100

    day 8

---
+++ 2025/Babulle.AdventOfCode2025.Day8/Babulle.AdventOfCode2025.Day8/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace Babulle.AdventOfCode2025.Day8;
+
+public class Node(int i, int j, float distance)
+{
+    public float Distance { get; set; } = distance;
+    
+    public int I { get; } = i;
+    public int J { get; } = j;
+}
---
+++ 2025/Babulle.AdventOfCode2025.Day8/Babulle.AdventOfCode2025.Day8/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+using Babulle.AdventOfCode2025.Day8;
+
+var lines = File.ReadAllLines("input.txt");
+const int n = 1000;
+var l = lines.Length;
+
+var positions = lines.Select(line => line.Split(',')).Select(coordinates => new Vector3(int.Parse(coordinates[0]), int.Parse(coordinates[1]), int.Parse(coordinates[2]))).ToList();
+
+var nodes = new List<Node>();
+
+var groups = new List<List<int>>();
+
+for (var i = 0; i < l; ++i)
+{
+    for (var j = i + 1; j < l; ++j)
+    {
+        var distance = Vector3.Distance(positions[i], positions[j]);
+        nodes.Add(new Node(i, j, distance));
+    }
+}
+
+nodes = nodes.OrderBy(node => node.Distance).ToList();
+
+var k = 0;
+var result1 = 0;
+var node = new Node(0, 0, 0);
+
+while (!(groups.Count == 1 && groups[0].Count == l))
+{
+    if (k == n)
+    {
+        result1 = groups.OrderByDescending(group => group.Count)
+            .Select(group => group.Count)
+            .Take(3)
+            .Aggregate((a, b) => a * b);
+    }
+    
+    node = nodes[k];
+    
+    var iGroup = groups.FirstOrDefault(group => group.Contains(node.I));
+    var jGroup = groups.FirstOrDefault(group => group.Contains(node.J));
+
+    if (iGroup == null && jGroup == null)
+    {
+        List<int> group = [node.I, node.J];
+        groups.Add(group);
+    }
+    else if (iGroup != null && jGroup == null)
+    {
+        iGroup.Add(node.J);
+    }
+    else if (iGroup == null && jGroup != null)
+    {
+        jGroup.Add(node.I);
+    }
+    else if (iGroup != null && jGroup != null && iGroup != jGroup)
+    {
+        groups.Remove(jGroup);
+        iGroup.AddRange(jGroup);
+    }
+
+    ++k;
+}
+
+Console.WriteLine(result1);
+Console.WriteLine((int) positions[node.I].X * (int) positions[node.J].X);
